ec52ca10019a9b1428dca57960996b9bd99defbe
batyr <b.bobetayev.97@gmail.com>
2020-11-08 16:57:28 +0100

    endpoints

--- src/store/ApiStore.ts
+++ src/store/ApiStore.ts
@@ -1,12 +1,8 @@
 import { observable, action } from 'mobx'
 import apiCall from '../utils/apiCall'
-type customer = {
-  id: string,
-  name: string,
-  surname: string,
-  created: string,
-}
-type responseCustomers = {
+import { customer, event, customerEvent, source } from './DataStore'
+
+export type responseCustomers = {
   customers: customer[],
   selection_settings: {
     offset: number,
@@ -14,9 +10,22 @@ type responseCustomers = {
     search: string
   }
 }
+
 class ApiStore {
   @action 
   fetchCustomers = ():Promise<responseCustomers> => apiCall('customers')
+
+  @action 
+  fetchCustomerById = (id:string):Promise<customer> => apiCall(`customers/${id}`)
+
+  @action 
+  fetchEvents = ():Promise<event[]> => apiCall('events')
+
+  @action 
+  fetchCustomerEvents = (id:string):Promise<customerEvent[]> => apiCall(`customers/${id}`)
+    
+  @action 
+  fetchSources = ():Promise<source[]> => apiCall('sources')
 }
 
 export default new ApiStore()
--- src/store/DataStore.ts
+++ src/store/DataStore.ts
@@ -2,23 +2,40 @@ import { observable, action } from 'mobx'
 import apiCall from '../utils/apiCall'
 import ApiStore from './ApiStore'
 
-type customer = {
+export type customer = {
   id: string,
   name: string,
   surname: string,
   created: string,
 }
-type responseCustomers = {
-  customers: customer[],
-  selection_settings: {
-    offset: number,
-    limit: number,
-    search: string
+
+export type customerEvent = {
+  id: string,
+  event_id: string,
+  datetime: string
+}
+
+export type event = {
+  id: string,
+  source_id : string,
+  title : string,
+  short_title : string
+}
+
+export type source = {
+  id: string,
+  name : string,
+  frontend_settings : {
+    icon: string,
+    color: string
   }
 }
+
 class DataStore {
   @observable customers:customer[] = []
-  @observable customer = {}
+  @observable events:event[] = []
+  @observable customerEvents:customerEvent[] = []
+  @observable sources:source[] = []
 
   @action 
   setCustomers = async () => {
@@ -26,6 +43,11 @@ class DataStore {
     this.customers = res.customers
   }
 
+  @action 
+  setEvents = async () => {
+    const res = await ApiStore.fetchEvents()
+    this.events = res
+  }
 }
 
 export default new DataStore()
