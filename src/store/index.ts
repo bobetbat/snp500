0c8a660838ca0682ed4bdd791f0117a64f99e499
batyr <b.bobetayev.97@gmail.com>
2020-11-08 13:35:00 +0100

    mobx observer fixed

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,9 @@ import { observable, action } from 'mobx'
 import DataStore from './DataStore'
 import ApiStore from './ApiStore'
 
-class AppStore {
-  data = DataStore
-  api = ApiStore
+const AppStore = {
+  data: DataStore,
+  api: ApiStore
 }
 
-export default new AppStore()
+export default AppStore
